c4ebf6aac360c03a44cf04356af38f668c7b1113
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-12-31 17:20:44 +0530

    introducing functions

--- src/main/java/com/mgunawardhana/s3/config/StorageConfig.java
+++ src/main/java/com/mgunawardhana/s3/config/StorageConfig.java
@@ -1,5 +1,12 @@
 package com.mgunawardhana.s3.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,4 +17,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StorageConfig {
+
+    @Value("${cloud.aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.s3.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.region.static}")
+    private String region;
+
+    @Bean
+    protected AmazonS3 getAmazonS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region).build();
+    }
 }
