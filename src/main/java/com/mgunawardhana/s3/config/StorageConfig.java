86f9095fd1d3742e6586b01e585fcc03bde8a9cc
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-12-31 14:35:41 +0530

    introducing project

---
+++ src/main/java/com/mgunawardhana/s3/config/StorageConfig.java
@@ -0,0 +1,13 @@
+package com.mgunawardhana.s3.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created By - mgunawardhana
+ * Date - 2023-12-31
+ * Time - 14.34
+ */
+
+@Configuration
+public class StorageConfig {
+}
